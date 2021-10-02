910e955382e84b5cbe4bb4331c44e73fa84f18cb
Nicole-favela <47462513+Nicole-favela@users.noreply.github.com>
2021-10-01 20:30:21 -0600

    Add files via upload

---
+++ kermoss-fixes/CommandSchedulerIT copy.java
@@ -0,0 +1,95 @@
+package io.kermoss.cs.task;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.moreThanOrExactly;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
+
+import io.kermoss.cs.domain.Command;
+import io.kermoss.cs.service.CommandService;
+
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("test")
+public class CommandSchedulerIT {
+    @Autowired
+    private CommandService commandService;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @ClassRule
+    //NEF design violation: mutable static state 
+    public static final WireMockClassRule wiremock = new WireMockClassRule(wireMockConfig().dynamicPort());
+
+    @Before
+    public void setup() throws IOException {
+        jdbcTemplate.execute("CREATE TABLE KERMOSS_CMD (id VARCHAR(50), status VARCHAR(25), startedTimestamp INTEGER)");
+        stubFor(post(urlMatching("//command-executor/commands/.*/restart"))
+            .willReturn(
+                aResponse()
+                    .withStatus(HttpStatus.CREATED.value())
+                    .withUniformRandomDelay(0, 2000)
+                    .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+            )
+        );
+    }
+
+    @Test
+    public void shouldRestartFailedCommand() {
+        final Command cmd = this.createFailedCommand();
+
+        try {
+            Thread.sleep(5000);
+        } catch (final InterruptedException theException) {}
+
+        verify(moreThanOrExactly(1), postRequestedFor(urlEqualTo(String.format("//command-executor/commands/%s/restart", cmd.getId()))));
+    }
+
+    @Test
+    public void shouldRestartStartedTimedOutCommand() {
+        final Command cmd = this.createStartedTimedOutCommand();
+
+        try {
+            Thread.sleep(5000);
+        } catch (final InterruptedException theException) {}
+
+        verify(moreThanOrExactly(1), postRequestedFor(urlEqualTo(String.format("//command-executor/commands/%s/restart", cmd.getId()))));
+    }
+
+    
+
+    private Command createStartedTimedOutCommand() {
+        final Command cmd = new Command(UUID.randomUUID().toString(), "STARTED");
+        this.jdbcTemplate.execute(String.format("INSERT INTO KERMOSS_CMD(id, status, startedTimestamp) VALUES('%s', '%s', '0')", cmd.getId(), cmd.getStatus()));
+        return cmd;
+    }
+
+    private Command createFailedCommand() {
+        final Command cmd = new Command(UUID.randomUUID().toString(), "FAILED");
+        this.jdbcTemplate.execute(String.format("INSERT INTO KERMOSS_CMD(id, status) VALUES('%s', '%s')", cmd.getId(), cmd.getStatus()));
+        return cmd;
+    }
+}
---
+++ kermoss-fixes/DefaultPrepareCommandTransporterTest copy.java
@@ -0,0 +1,146 @@
+package io.kermoss.cmd.infra.transporter;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.nullable;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import io.kermoss.cmd.app.CommandOrchestrator;
+import io.kermoss.cmd.app.CommandOrchestratorImpl;
+import io.kermoss.cmd.domain.CommandMeta;
+import io.kermoss.cmd.domain.OutboundCommand;
+import io.kermoss.cmd.domain.TransporterCommand;
+import io.kermoss.cmd.domain.event.OutboundCommandPrepared;
+import io.kermoss.cmd.domain.repository.CommandRepository;
+import io.kermoss.cmd.infra.transporter.strategies.CommandTransporterStrategy;
+import io.kermoss.cmd.infra.transporter.strategies.HttpClientStrategy;
+import io.kermoss.infra.KermossTxLogger;
+
+@MockitoSettings(strictness = Strictness.LENIENT)
+public class DefaultPrepareCommandTransporterTest {
+
+    @Mock
+    private Environment mockEnvironment;
+    @Mock
+    //NEF
+    //fix for codestyle LongVariable
+    private CommandRepository mockCmdRep;
+    @Mock
+    private CommandOrchestrator mockCommandOrchestrator;
+    @Mock
+    private RestTemplate mockRestTemplate;
+    @Mock
+    private HttpClientStrategy clientStrategy;
+    @Mock
+    KermossTxLogger txLogger;
+    
+
+    @InjectMocks
+    private DefaultPrepareCommandTransporter prepareRestCommandTransporter;
+
+    
+    @BeforeEach
+    public void setUp() {
+        //initMocks(this);
+
+    }
+
+    @Test
+    public void testOnEventWhenPostIs2xx() {
+        // Setup
+        final OutboundCommandPrepared event = mock(OutboundCommandPrepared.class);
+        //when(event.getId()).thenReturn(UUID.randomUUID().toString());
+        final CommandMeta meta = mock(CommandMeta.class);
+        final OutboundCommand command = mock(OutboundCommand.class);
+        final ResponseEntity entity = mock(ResponseEntity.class);
+        
+        CommonSetup(event, meta, command);
+        CommandTransporterStrategy strategy = mock(CommandTransporterStrategy.class);  
+        when(clientStrategy.get()).thenReturn(strategy);
+        when(strategy.transportPrepareCommand(any())).thenReturn(true);
+        when(mockRestTemplate.exchange(nullable(String.class), any(), any(), same(TransporterCommand.class))).thenReturn(entity);
+        when(entity.getStatusCode()).thenReturn(HttpStatus.OK);
+        when(txLogger.printJsonObject(any())).thenReturn("Command String");
+        // Run the test
+        prepareRestCommandTransporter.onEvent(event);
+
+        // Verify the results
+        verifyTheCommonResults(command);
+        verify(mockCommandOrchestrator).receive(any(OutboundCommand.class));
+    }
+
+    
+
+    @Test
+    public void testOnEventWhenCommandStatusNotPrepared() {
+        // Setup
+        final OutboundCommandPrepared event = mock(OutboundCommandPrepared.class);
+        final CommandMeta meta = mock(CommandMeta.class);
+        final OutboundCommand command = mock(OutboundCommand.class);
+
+        CommonSetup(event, meta, command);
+        when(command.getStatus()).thenReturn(OutboundCommand.Status.STARTED);
+
+        // Run the test
+        prepareRestCommandTransporter.onEvent(event);
+
+        // Verify the results
+        verify(mockEnvironment, never()).getProperty(nullable(String.class));
+        verify(command, never()).setSource(nullable(String.class));
+        verify(mockCommandOrchestrator, never()).receive(any(OutboundCommand.class));
+    }
+
+    @Test
+    public void testOnEventWhenCommandNotFoundInDb() {
+        // Setup
+        final OutboundCommandPrepared event = mock(OutboundCommandPrepared.class);
+        final CommandMeta meta = mock(CommandMeta.class);
+        final OutboundCommand command = mock(OutboundCommand.class);
+
+        when(event.getMeta()).thenReturn(meta);
+        when(mockCommandRepository.findOutboundCommandOpt(nullable(String.class))).thenReturn(Optional.empty());
+
+        // Run the test
+        prepareRestCommandTransporter.onEvent(event);
+
+        // Verify the results
+        verify(mockEnvironment, never()).getProperty(nullable(String.class));
+        verify(command, never()).setSource(nullable(String.class));
+        verify(mockCommandOrchestrator, never()).receive(any(OutboundCommand.class));
+    }
+
+    private void CommonSetup(OutboundCommandPrepared event, CommandMeta meta, OutboundCommand command) {
+        when(event.getMeta()).thenReturn(meta);
+        when(mockCommandRepository.findOutboundCommandOpt(nullable(String.class))).thenReturn(Optional.of(command));
+        when(command.getStatus()).thenReturn(OutboundCommand.Status.PREPARED);
+        when(mockEnvironment.getProperty(same("kermoss.service-name"), nullable(String.class))).thenReturn("source");
+        when(mockEnvironment.getProperty(same("spr"), nullable(String.class))).thenReturn("source");
+    }
+
+    private void verifyTheCommonResults(OutboundCommand command) {
+        verify(command).setSource(same("source"));
+    }
+}
